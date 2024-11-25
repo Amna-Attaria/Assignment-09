c23ef04b710c5a4b20af76770f3eefb2c950d77c
Amna-Attaria <khan12amna26@gmail.com>
2024-11-25 20:38:15 +0500

    JS Assignment chapter42-48

---
+++ app.js
@@ -0,0 +1,17 @@
+// Qno:01
+// function power(a,b)
+// {
+//     if(b===0)
+//     {
+//         return 1
+//     }
+//     var result = a
+//    for(var i =1; i<b; i++)
+//    {
+//     result*=a
+//    }
+//    return result
+// }
+// alert(power(2,3))
+
+// Qno:0
